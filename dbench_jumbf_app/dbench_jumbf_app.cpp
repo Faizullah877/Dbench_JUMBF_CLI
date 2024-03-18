08119d84d0d94a1092d8e7e2e7c6450ede32e701
faiz_sejong <faiz@sju.ac.kr>
2024-03-18 16:51:55 +0900

    Add project files.

---
+++ dbench_jumbf_app/dbench_jumbf_app.cpp
@@ -0,0 +1,12 @@
+﻿// dbench_jumbf_app.cpp : Defines the entry point for the application.
+//
+
+#include "dbench_jumbf_app.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
